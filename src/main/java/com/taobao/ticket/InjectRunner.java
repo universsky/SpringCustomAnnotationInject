dcbedaf9efeed1d6eea77e175a8cf06cfb715fe5
Nathan Liu <nathanlhb@gmail.com>
2013-10-22 13:44:17 +0800

    update @HSFConsumer

--- src/main/java/com/taobao/ticket/InjectRunner.java
+++ src/main/java/com/taobao/ticket/InjectRunner.java
@@ -17,8 +17,18 @@ public class InjectRunner {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
                 springResourcePath);
 
+
         UicReadDecorator reader = ctx.getBean(UicReadDecorator.class);
         BaseUserDO user = reader.getUser(10000L);
-        System.out.println("user[id:10000L] nick:" + user.getNick());
+        System.out.println("user[id:10000L] nick:" + user);
+
+
+        /*UicReadService uicReadService = (UicReadService) ctx
+                .getBean("uicReadServiceXML");
+
+        ServiceUtil.waitServiceReady(uicReadService);//不是必须，因为运行很快，所以要在configServer推地址下来前检测地址是否推下來，可以自己sleep(500)
+        BaseUserDO user = uicReadService.getBaseUserByUserId(10000L, "detail")
+                .getModule();
+        System.out.println("user[id:10000L] nick:" + user.getNick());*/
     }
 }
--- src/main/java/com/taobao/ticket/UicReadDecorator.java
+++ src/main/java/com/taobao/ticket/UicReadDecorator.java
@@ -4,10 +4,14 @@ import com.alibaba.dxp.hsfext.annotation.HSFConsumer;
 import com.taobao.hsf.hsfunit.util.ServiceUtil;
 import com.taobao.uic.common.domain.BaseUserDO;
 import com.taobao.uic.common.service.userinfo.UicReadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @auther: zheshan
@@ -16,19 +20,31 @@ import javax.annotation.Resource;
 @Service
 public class UicReadDecorator {
     @HSFConsumer
+    @Qualifier("uicReadService")
+    @Autowired
     private UicReadService uicReadService;
 
-    @HSFConsumer(interfaceName = "uicReadService2")
+
+    @HSFConsumer
+    @Qualifier("uicReadService2")
+    @Autowired
     private UicReadService uicReadService2;
+
+
     //支持使用xml配置文件的方式来注入
-    @HSFConsumer(interfaceName = "uicReadServiceXML")
+    @Qualifier("uicReadServiceXML")
+    @HSFConsumer
+    @Autowired
     private UicReadService uicReadServiceXML;
 
     //使用xml配置文件的方式来注入
-    @Resource(name = "uicReadServiceXML")
+    @Resource(name = "uicReadServiceXML2")
     private UicReadService uicReadServiceXML2;
+
     public BaseUserDO getUser(long userId){
         //不是必须，因为运行很快，所以要在configServer推地址下来前检测地址是否推下來，可以自己sleep(500)
+        Map<String,Object> test = new HashMap<String,Object>();
+        System.out.println(test.values().contains(uicReadService));
         ServiceUtil.waitServiceReady(uicReadService);
         BaseUserDO user = uicReadService.getBaseUserByUserId(userId, "detail")
                 .getModule();
@@ -39,7 +55,9 @@ public class UicReadDecorator {
         }catch (Exception e){
             e.printStackTrace();
         }
+
         return user;
+//        return null;
     }
     @PostConstruct
     public void init(){
